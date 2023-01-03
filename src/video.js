e4d85f8e2837b565086b5727b4f0b8b8694fd529
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2023-01-03 11:53:04 -0800

    work on video

--- src/video.js
+++ src/video.js
@@ -1,1 +1,11 @@
-export default async function generateVideo(images, text, output) {}
+import fs from "fs";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
+const ffmpeg = createFFmpeg({ log: true });
+export default async function generateVideo(images, text, output) {
+    await ffmpeg.load();
+    ffmpeg.FS("writeFile", "audio.wav", await fetchFile("./audio.wav"));
+    for (let i = 0; i < 60; i++) {
+        const num = `00${i}`.slice(-3);
+    }
+}
