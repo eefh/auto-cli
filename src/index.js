5a34f40de9bd333b1e37fe0d9c405e728e3ab909
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2023-01-02 22:06:23 -0800

    Add Text to speech functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Command } from "commander";
 import getStory from "./gpt.js";
 import generateImages from "./image.js";
-
+import generateSpeech from "./text-to-speech.js";
 const program = new Command();
 program
     .option("-p, --prompt <prompt>", "Story prompt")
@@ -14,10 +14,12 @@ const main = async () => {
     console.log("Generating story...");
     const story = await getStory(options.prompt);
     await console.log(story.text);
-    console.log("Generating images...");
+    /*console.log("Generating images...");
     const images = await generateImages(story.imagePrompts);
     await images.forEach((url) => {
         console.log(url);
-    });
+    });*/
+    console.log("Generating speech...");
+    generateSpeech(story.text);
 };
 main();
---
+++ src/text-to-speech.js
@@ -0,0 +1,39 @@
+import sdk from "microsoft-cognitiveservices-speech-sdk";
+
+export default function generateSpeech(text) {
+    let audioFile = "audio.wav";
+
+    const speechConfig = sdk.SpeechConfig.fromSubscription(
+        process.env.SPEECH_KEY,
+        process.env.SPEECH_REGION
+    );
+    const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
+    speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural";
+
+    var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+    // Start the synthesizer and wait for a result.
+
+    // Start the synthesizer and wait for a result.
+    synthesizer.speakTextAsync(
+        text,
+        (result) => {
+            if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+                console.log("Voice synthesis finished.");
+            } else {
+                console.error(
+                    "Speech synthesis canceled, " +
+                        result.errorDetails +
+                        "\nDid you set the speech resource key and region values?"
+                );
+            }
+            synthesizer.close();
+            synthesizer = null;
+        },
+        (err) => {
+            console.trace("err - " + err);
+            synthesizer.close();
+            synthesizer = null;
+        }
+    );
+    console.log("Now synthesizing to: " + audioFile);
+}
